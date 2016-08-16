45447d774cb72f5ff3d36559a1f04f2a7b7c7ad4
Jackson Behan <jb681831@gmail.com>
2016-08-16 12:47:50 -0700

    full screen dropdown nav for tablet/mobile

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,26 @@
+var navItemHeight;
+$( document ).ready(function() {
+    var height = window.innerHeight;
+    var navHeight = height - 50;
+    navItemHeight = navHeight/4
+    // console.log(navItemHeight)
+});
+
 $(function () {
     $('.headline').textillate();
 })
+
+$('#nav-button').click(function() {
+  $('.headline').addClass('hidden');
+  $('#nav-button').addClass('hidden');
+  $('.fa-times').removeClass('hidden');
+  $('.navbar-nav li').css('height', navItemHeight)
+});
+
+$('.fa-times').click(function() {
+  $('.navbar-collapse').removeClass('in');
+  $('.headline').removeClass('hidden');
+  $('.fa-times').addClass('hidden');
+  $('#nav-button').removeClass('hidden');
+  $('.navbar-nav li').css('height', 50)
+});
